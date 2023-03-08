f61023478b4e5ea1079819db60bd670252d2df1f
farkov99raven <petkofarkov@gmail.com>
2023-03-08 18:02:42 +0200

    Update 05.monthPrinter.js

--- Basic Syntax, Conditional Statements and Loops/05.monthPrinter.js
+++ Basic Syntax, Conditional Statements and Loops/05.monthPrinter.js
@@ -1,14 +1,14 @@
 function monthPrinter(number) {
     switch (number) {
-        case 1: console.log(`January`); break; // first month
+        case 1: console.log(`January`); break;
         case 2: console.log(`February`); break;
         case 3: console.log(`March`); break;
         case 4: console.log(`April`); break;
-        case 5: console.log(`May`); break; // 5th month
+        case 5: console.log(`May`); break;
         case 6: console.log(`June`); break;
         case 7: console.log(`July`); break;
         case 8: console.log(`August`); break;
-        case 9: console.log(`September`); break; // 9th month
+        case 9: console.log(`September`); break;
         case 10: console.log(`October`); break;
         case 11: console.log(`November`); break;
         case 12: console.log(`December`); break;
